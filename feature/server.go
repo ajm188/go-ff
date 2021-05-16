4387bca7ec503ba9e42dd05276302535f59dbc66
Andrew Mason <amason@hey.com>
2021-05-15 21:05:42 -0400

    Sort features (relatively) naively

--- feature/server.go
+++ feature/server.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"sort"
 	"strings"
 	"sync"
 	"text/template"
@@ -214,8 +215,76 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp, _ := inst.GetFeatures(r.Context(), &featurepb.GetFeaturesRequest{})
+	sort.SliceStable(resp.Features, func(i, j int) bool {
+		left, right := resp.Features[i], resp.Features[j]
+		return compare(left, right)
+	})
 	if err := indexTmpl.Execute(w, resp); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 	}
+}
+
+func compare(left, right *featurepb.Feature) bool {
+	leftEnabled := isNaivelyEnabled(left)
+	rightEnabled := isNaivelyEnabled(right)
+
+	if leftEnabled {
+		if rightEnabled {
+			if left.Type == right.Type {
+				if left.Type == featurepb.Feature_PERCENTAGE_BASED {
+					return (left.Percentage > right.Percentage) || (left.Name < right.Name)
+				}
+
+				return left.Name < right.Name
+			}
+
+			// Constants go before PBR
+			if left.Type == featurepb.Feature_CONSTANT {
+				return true
+			}
+
+			if right.Type == featurepb.Feature_CONSTANT {
+				return false
+			}
+
+			// Technically, since isNaivelyEnabled can only return true for
+			// CONSTANT and PERCENTAGE_BASED types, this is unreachable.
+		}
+
+		return true
+	}
+
+	if rightEnabled {
+		return false
+	}
+
+	if left.Type == right.Type {
+		return left.Name < right.Name
+	}
+
+	switch left.Type {
+	case featurepb.Feature_CONSTANT:
+		return true
+	case featurepb.Feature_PERCENTAGE_BASED:
+		switch right.Type {
+		case featurepb.Feature_CONSTANT:
+			return false
+		default:
+			return true
+		}
+	default:
+		return false
+	}
+}
+
+func isNaivelyEnabled(f *featurepb.Feature) bool {
+	switch f.Type {
+	case featurepb.Feature_CONSTANT:
+		return f.Enabled
+	case featurepb.Feature_PERCENTAGE_BASED:
+		return f.Percentage > 0
+	}
+
+	return false
 }
