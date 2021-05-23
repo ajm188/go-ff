a86fd9809c2afc4a54240b31a657d61f857acbfd
Andrew Mason <amason@hey.com>
2021-05-22 20:18:32 -0400

    Rework the `Read` implementation to do more look-aheads and bulk copies
    This takes the performance hit of unescaping from 2 orders of magnitude
    worse than an `io.Copy` with no modifications down to 1 order of
    magnitude worse.

--- internal/jsonutil/unescaper.go
+++ internal/jsonutil/unescaper.go
@@ -24,27 +24,55 @@ func NewHTMLUnescaper(buf []byte) *HTMLUnescaper {
 
 func (u *HTMLUnescaper) Read(b []byte) (int, error) {
 	n := 0
+	i := 0
 
-	for i := 0; i < len(b); i++ {
+	for i < len(b) {
 		if u.offset >= len(u.buf) {
 			return n, io.EOF
 		}
 
-		if len(u.buf)-u.offset < 6 {
-			b[i] = u.buf[u.offset]
-			n++
-			u.offset++
-			continue
+		if len(u.buf[u.offset:]) < 6 {
+			x := copy(b[i:], u.buf[u.offset:])
+			n += x
+			u.offset += x
+			goto done
+		}
+
+		j := bytes.Index(u.buf[u.offset:], []byte(`\u`))
+		if j == -1 {
+			// no escaped characters remaining in u.buf, just copy everything
+			x := copy(b[i:], u.buf[u.offset:])
+			n += x
+			u.offset += x
+			goto done
+		}
+
+		if j > 0 {
+			// escaped character is not the front of u.buf[u.offset:], copy
+			// everything up to it first
+			x := copy(b[i:], u.buf[u.offset:u.offset+j])
+			i += x
+			n += x
+			u.offset += x
+		}
+
+		if i >= len(b) {
+			// We've filled the destination buffer
+			goto done
+		}
+
+		if len(u.buf[u.offset:]) < 6 {
+			// even though we saw the beginning of an escape sequence, there's
+			// not even remaining characters to have a real escape sequence.
+			// this is a duplication of the block at the beginning of the copy
+			// loop
+			x := copy(b[i:], u.buf[u.offset:])
+			n += x
+			u.offset += x
+			goto done
 		}
 
 		sextet := u.buf[u.offset : u.offset+6]
-		if !bytes.HasPrefix(sextet, []byte(`\u`)) {
-			b[i] = u.buf[u.offset]
-			n++
-			u.offset++
-			continue
-		}
-
 		switch 0 {
 		case bytes.Compare(sextet[2:], leftAngle):
 			b[i] = '<'
@@ -59,10 +87,19 @@ func (u *HTMLUnescaper) Read(b []byte) (int, error) {
 			n++
 			u.offset += 6
 		default:
+			// Best we can do is take a single byte and go to the beginning of
+			// the loop to rescan for the next potential escape sequence
 			b[i] = u.buf[u.offset]
 			n++
 			u.offset++
 		}
+
+		i++
+	}
+
+done:
+	if u.offset >= len(u.buf) {
+		return n, io.EOF
 	}
 
 	return n, nil
